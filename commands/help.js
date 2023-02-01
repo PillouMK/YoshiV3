095db57f686f5c27b2ee77d0f28057795c4e2f4c
PillouMK <maxence.pille@gmail.com>
2023-02-01 13:11:20 +0100

    fix bug for help command

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 const fs = require('fs');
 
 /**
@@ -10,142 +11,123 @@ const fs = require('fs');
 
 module.exports.run = async (bot, message, args) =>
 {
-    
-    const msg1 = {
-        embed: {
-            color: 3066993,
-            title: "__**Bot-war:**__",
-            fields: [
-                {
-                    name: "**!startwar X Y :**",
-                    value: "Débute un war entre les teams X et Y",
-                },
-                {
-                    name: "**!race X X X X X X Y**",
-                    value: "Ajoute une course, \"X\" correspond aux 6 places de l'équipe, \"Y\" à la map",
-                },
-                {
-                    name: "**!er X X X X X X N MAP**",
-                    value: "Permet d'éditer une course, \"N\" est le numéro de la course, \"MAP\" la map et \"X\" les 6 placements",
-                },
-                {
-                    name: "**!set_team [ROSTER]**",
-                    value: "Permet de modifier le tag de la team pour le bot-war (YFG et YFO activent la sauvegarde des données)",
-                },
-                {
-                    name: "**!pena YF** ou **!pena adv**",
-                    value: "Permet d'ajouter une pénalité de 20 points à l'une des équipes, YF et adv correspondent à l'équipe visée",
-                },
-                {
-                    name: "**!stopwar**",
-                    value: "Met fin au war du channel courant",
-                }
-            ],
-            footer: {
-                text: `Guide Bot-war`,
-            } 
-        }
-    };
+    const embed1 = new EmbedBuilder()
+        .setColor(3066993)
+        .setTitle("__**Bot-war:**__")
+        .addFields(
+            { 
+                name: "**!startwar X Y :**", 
+                value: "Débute un war entre les teams X et Y" },
+            {
+                name: "**!race X X X X X X Y**",
+                value: "Ajoute une course, \"X\" correspond aux 6 places de l'équipe, \"Y\" à la map",
+            },
+            {
+                name: "**!er X X X X X X N MAP**",
+                value: "Permet d'éditer une course, \"N\" est le numéro de la course, \"MAP\" la map et \"X\" les 6 placements",
+            },
+            {
+                name: "**!set_team [ROSTER]**",
+                value: "Permet de modifier le tag de la team pour le bot-war (YFG et YFO activent la sauvegarde des données)",
+            },
+            {
+                name: "**!pena YF** ou **!pena adv**",
+                value: "Permet d'ajouter une pénalité de 20 points à l'une des équipes, YF et adv correspondent à l'équipe visée",
+            },
+            {
+                name: "**!stopwar**",
+                value: "Met fin au war du channel courant",
+            }
+        )
+        .setFooter({text: `Guide Bot-war`})
 
-        const msg2 = {
-            embed: {
-                color: 3066993,
-                title: "__**Code-ami et enregistrement de code-ami**__",
-                fields: [
-                    {
-                    name: "**!fc**",
-                    value: "Renvoi ton code-ami s'il est enregistré",
-                    },
-                    {
-                    name: "**!fc @membre**",
-                    value: "Renvoi le code-ami du joueur taggé s'il est enregistré",
-                    },
-                    {
-                    name: "**!setfc xxxx-xxxx-xxxx**",
-                    value: "Enregistre ton code-ami, ATTENTION: le code-ami doit être au format xxxx-xxxx-xxxx",
-                    },
-                    {
-                    name: "**!setfc @membre xxxx-xxxx-xxxx**",
-                    value: "Enregistre le code-ami du joueur mentionné, ATTENTION: le code-ami doit être au format xxxx-xxxx-xxxx",
-                    }
-                ],
-                footer: {
-                    text: `Guide Code-ami`,
-                } 
-            }
-        };
-       const msg3 = {
-            embed: {
-                color: 3066993,
-                title: "__**Commande Line up **__",
-                fields: [
-                    {
-                    name: "**!can [horaire]**",
-                    value: "Permet de s'enregistrer pour l'horaire voulu (possibilité de mettre plusieurs horaires)",
-                    },
-                    {
-                    name: "**!can @membre [horaire]**",
-                    value: "Permet d'enregistrer le membre mentionné pour un horaire voulu",
-                    },
-                    {
-                    name: "**!maybe [horaire]**",
-                    value: "Permet de s'enregistrer pour l'horaire voulu en tant que maybe (possibilité de mettre plusieurs horaires)",
-                    },
-                    {
-                    name: "**!m @membre [horaire]**",
-                    value: "Permet d'enregistrer le membre mentionné pour un horaire voulu en tant que maybe",
-                    },
-                    {
-                    name: "**!drop [horaire]**",
-                    value: "Permet de se désister d'un horaire (un seul horaire par commande)",
-                    },
-                    {
-                    name: "**!d @membre [horaire]**",
-                    value: "Permet de désister le membre mentionné d'un horaire (un seul horaire par commande)",
-                    },
-                ],
-                footer: {
-                    text: `Guide line up`,
-                } 
-            }
-        };
-        const msg4 = {
-            embed: {
-                color: 3066993,
-                title: "__**Commande Timetrial **__",
-                fields: [
-                    {
-                    name: "**!set_tt [MAP] [TIME]**",
-                    value: "Permet d'enregistrer un nouveau temps'",
-                    },
-                    {
-                    name: "**!classement [MAP]**",
-                    value: "Permet de visionner un classement de la map [MAP]",
-                    },
-                    {
-                        name: "**!new_name [name]**",
-                        value: "Permet de modifier ton pseudo pour la base de données (15 caractères limité, pas de caractères spécial ou emoji)",
-                    }
-                ],
-                footer: {
-                    text: `Guide Timetrial`,
-                } 
-            }
-        };
+        const embed2 = new EmbedBuilder()
+        .setColor(3066993)
+        .setTitle("__**Code-ami et enregistrement de code-ami**__")
+        .addFields(
+                {
+                name: "**!fc**",
+                value: "Renvoi ton code-ami s'il est enregistré",
+                },
+                {
+                name: "**!fc @membre**",
+                value: "Renvoi le code-ami du joueur taggé s'il est enregistré",
+                },
+                {
+                name: "**!setfc xxxx-xxxx-xxxx**",
+                value: "Enregistre ton code-ami, ATTENTION: le code-ami doit être au format xxxx-xxxx-xxxx",
+                },
+                {
+                name: "**!setfc @membre xxxx-xxxx-xxxx**",
+                value: "Enregistre le code-ami du joueur mentionné, ATTENTION: le code-ami doit être au format xxxx-xxxx-xxxx",
+                }
+        )
+        .setFooter({text: `Guide Code-ami`})
+
+        const embed3 = new EmbedBuilder()
+        .setColor(3066993)
+        .setTitle("__**Commande Line up **__")
+        .addFields(
+                {
+                name: "**!can [horaire]**",
+                value: "Permet de s'enregistrer pour l'horaire voulu (possibilité de mettre plusieurs horaires)",
+                },
+                {
+                name: "**!can @membre [horaire]**",
+                value: "Permet d'enregistrer le membre mentionné pour un horaire voulu",
+                },
+                {
+                name: "**!maybe [horaire]**",
+                value: "Permet de s'enregistrer pour l'horaire voulu en tant que maybe (possibilité de mettre plusieurs horaires)",
+                },
+                {
+                name: "**!maybe @membre [horaire]**",
+                value: "Permet d'enregistrer le membre mentionné pour un horaire voulu en tant que maybe",
+                },
+                {
+                name: "**!drop [horaire]**",
+                value: "Permet de se désister d'un horaire (un seul horaire par commande)",
+                },
+                {
+                name: "**!drop @membre [horaire]**",
+                value: "Permet de désister le membre mentionné d'un horaire (un seul horaire par commande)",
+                },
+        )
+        .setFooter({text: `Guide line up`})
+
+        const embed4 = new EmbedBuilder()
+        .setColor(3066993)
+        .setTitle("__**Commande Timetrial **__")
+        .addFields(
+            {
+                name: "**!set_tt [MAP] [TIME]**",
+                value: "Permet d'enregistrer un nouveau temps'",
+                },
+                {
+                name: "**!classement [MAP]**",
+                value: "Permet de visionner un classement de la map [MAP]",
+                },
+                {
+                    name: "**!new_name [name]**",
+                    value: "Permet de modifier ton pseudo pour la base de données (15 caractères limité, pas de caractères spécial ou emoji)",
+                }
+        )
+        .setFooter({text: `Guide Timetrial`})
+        
         bot.users.fetch(message.author.id, false).then((user) => {
             user.send("__**Guide des commandes de YoshiV2**__\n");
         });
         bot.users.fetch(message.author.id, false).then((user) => {
-            user.send({embeds: [msg1]});
+            user.send({embeds: [embed1]});
         });
         bot.users.fetch(message.author.id, false).then((user) => {
-            user.send({embeds: [msg2]});
+            user.send({embeds: [embed2]});
         });
         bot.users.fetch(message.author.id, false).then((user) => {
-            user.send({embeds: [msg3]});
+            user.send({embeds: [embed3]});
         });
         bot.users.fetch(message.author.id, false).then((user) => {
-            user.send({embeds: [msg4]});
+            user.send({embeds: [embed4]});
         });
 }
 
